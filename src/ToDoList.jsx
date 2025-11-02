8d9f4056a19c6a1f650f8e50c44aca4a4644c6fd
Kseniya Serdiuk <serdiuk.xen@gmail.com>
2025-11-02 15:59:04 +0300

    add style

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function ToDoList() {
-    const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk a dog"]);
+    const [tasks, setTasks] = useState(["Позавтракать", "Принять душ", "Прогулка с собакой"]);
     const [newTask, setNewTask] = useState();
 
     function handleInputChange(event) {
@@ -10,44 +10,60 @@ function ToDoList() {
 
     function addTask() { }
 
-    function deleteTask(id) { }
+    function deleteTask(index) { }
 
-    function moveTaskUp(id) { }
+    function moveTaskUp(index) { }
 
-    function moveTaskDown(id) { }
+    function moveTaskDown(index) { }
 
     return (
-        <div>
+        <div className="text-center">
             <h1>ToDo Список</h1>
 
-            <div>
-                <input type="text"
-                    placeholder='Введите задачу ...'
-                    value={newTask}
-                    onChange={handleInputChange} />
-                <button 
-                    onClick={addTask}>
-                    Добавить
-                </button>
+            <div className="d-flex justify-content-center mt-4">
+                <div className="input-group mb-3 w-25">
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={newTask}
+                        onChange={handleInputChange}
+                        placeholder="Введите задачу ..."
+                    />
+                    <button
+                        className="btn btn-success"
+                        onClick={addTask}
+                        type="button"
+                    >
+                        Добавить
+                    </button>
+                </div>
             </div>
 
-            <ol>
+            <ol className="list-group list-group-numbered align-items-center">
                 {
                     tasks.map((task, index) =>
-                        <li key={index}>
-                            <span className="text">{task}</span>
-                            <button
-                                onClick={() => deleteTask(index)}>
-                                Удалить
-                            </button>
-                            <button
-                                onClick={() => moveTaskUp(index)}>
-                                Вверх
-                            </button>
-                            <button
-                                onClick={() => moveTaskDown(index)}>
-                                Вниз
-                            </button>
+                        <li key={index} className="list-group-item border-0 d-flex align-items-center gap-2 bg-light mb-3">
+                            <span className="fw-bold">{task}</span>
+                            <div className="d-flex gap-2">
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-danger"
+                                    onClick={() => deleteTask(index)}>
+                                    Удалить
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-primary"
+                                    onClick={() => moveTaskUp(index)}>
+                                    <i class="bi bi-arrow-up"></i>
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-outline-primary"
+                                    onClick={() => moveTaskDown(index)}>
+                                    <i class="bi bi-arrow-down"></i>
+                                </button>
+                            </div>
                         </li>
                     )}
             </ol>
