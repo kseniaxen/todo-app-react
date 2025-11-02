2d82c5b21638bcd535c10ec3330f189c3bd35f8a
Kseniya Serdiuk <serdiuk.xen@gmail.com>
2025-11-02 16:33:04 +0300

    Add logic for ToDoList

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -8,34 +8,60 @@ function ToDoList() {
         setNewTask(event.target.value)
     }
 
-    function addTask() { }
+    function addTask() {
+        if (newTask.trim() !== "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("");
+        }
+    }
 
-    function deleteTask(index) { }
+    function deleteTask(index) {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+    }
 
-    function moveTaskUp(index) { }
+    function moveTaskUp(index) {
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = 
+            [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
 
-    function moveTaskDown(index) { }
+    function moveTaskDown(index) {
+        if(index < tasks.length - 1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = 
+            [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
 
     return (
         <div className="text-center">
             <h1>ToDo Список</h1>
 
-            <div className="d-flex justify-content-center mt-4">
-                <div className="input-group mb-3 w-25">
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={newTask}
-                        onChange={handleInputChange}
-                        placeholder="Введите задачу ..."
-                    />
-                    <button
-                        className="btn btn-success"
-                        onClick={addTask}
-                        type="button"
-                    >
-                        Добавить
-                    </button>
+            <div className="container mt-4">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="input-group mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={newTask}
+                                onChange={handleInputChange}
+                                placeholder="Введите задачу ..."
+                            />
+                            <button
+                                className="btn btn-success"
+                                onClick={addTask}
+                                type="button"
+                            >
+                                Добавить
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
